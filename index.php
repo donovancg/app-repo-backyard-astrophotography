fc805367cbc638b9984c9607c14937ecdecca17d
Donovan Campbell-Gillies <dcgillies@Franks-Mac-mini.frontierlocal.net>
2018-05-08 15:22:39 -0700

    added files, began markup

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Backyard Astrophotography</title>
+</head>
+<body>
+    <header class="header">
+        <h1 class="heading--primary">Backyard Astrophotography</h1>
+        <div class="header__nav">
+            <nav class="nav">
+                <ul class="nav__ul">
+                    <li class="nav__li"><a href="#" class="nav__a nav__a--select u-link-no-style">Home</a></li>
+                    <li class="nav__li"><a href="#" class="nav__a u-link-no-style">Blog</a></li>
+                    <li class="nav__li"><a href="#" class="nav__a u-link-no-style">Gallery</a></li>
+                    <li class="nav__li"><a href="#" class="nav__a u-link-no-style">About</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+
+    </main>
+</body>
+</html>
