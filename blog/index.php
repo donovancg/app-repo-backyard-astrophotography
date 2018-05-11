a0bc7facd7ff931f077bd6b302f08927043b89c8
Donovan Campbell-Gillies <dcgillies@Franks-Mac-mini.frontierlocal.net>
2018-05-11 11:32:10 -0700

    added favicons

--- blog/index.php
+++ blog/index.php
@@ -4,6 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="../img/system/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../img/system/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../img/system/favicons/favicon-16x16.png">
+    <link rel="manifest" href="../img/system/favicons/site.webmanifest">
+    <link rel="shortcut icon" href="../img/system/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="msapplication-config" content="../img/system/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
     <link rel="stylesheet" href="../css/style.css">
     <title>Backyard Astrophotography</title>
 </head>
